a57b76e2c121f8b70e0d4a9ef9c13c3ea2002bc0
Kyle Hunter <nyan@furs.community>
2016-03-25 16:08:24 +1100

    update now

--- codepull/pull.sh
+++ codepull/pull.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 mkdir -p /root/tmp
 cd /root/tmp
-git clone https://github.com/HunterNyan/web-test.git
+if [ -d /root/tmp/web-test.git ]; then
+	cd web-test.git
+	git pull
+elif
+	git clone https://github.com/HunterNyan/web-test.git
+fi
 cd ./*
+rm -fr /var/www/*
 mv -f * /var/www/ 
